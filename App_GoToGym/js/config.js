f33ca9cf35a419f0c72a9e5c29d174f17b05d905
sebas21q <sebastian02vanegasrey@gmail.com>
2025-12-16 14:23:38 -0500

    Update API_URL for production backend

--- App_GoToGym/js/config.js
+++ App_GoToGym/js/config.js
@@ -3,5 +3,5 @@
 const isProd = window.location.hostname.includes("netlify.app");
 
 export const API_URL = isProd
-  ? "https://693b248a7fd925195cb9d0e8--moonlit-axolotl-97627d.netlify.app/" // backend en la nube
-  : "http://127.0.0.1:8000/api";                    // backend local para desarrollo
+  ? "https://pwagotofym.onrender.com/api"  // 👈 tu backend Django en Render
+  : "http://127.0.0.1:8000/api";           // backend local en tu PC
